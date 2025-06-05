8ed13a390c021411e929d322dacc9e07771985a2
gradgrind <mt.bothfeld@gmail.com>
2025-06-05 14:28:51 +0200

    Updates

--- minion_go/minion_test.go
+++ minion_go/minion_test.go
@@ -3,43 +3,71 @@ package minion
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"testing"
 	"time"
 )
 
 func TestM1(t *testing.T) {
 	fmt.Println("\n +++ TestM1:\n ==============")
-	doc, e := MinionRead("{A: 1 :}")
+	ib := InputBuffer{}
+	doc, e := ib.Read("{A: 1 :}")
 	if len(e) == 0 {
 		fmt.Println("  -->")
-		fmt.Println(MinionDump(doc.Item, -1))
+		dump := Dumper()
+		fmt.Println(dump(doc, -1))
 	} else {
 		fmt.Println(" *** Error ***")
 		fmt.Println(e)
 	}
 }
 
+var infiles = []string{
+	"../data/test1.minion",
+	//"../data/test1a.minion",
+	"../data/test2.minion",
+	//"../data/test2a.minion",
+	//"../data/test2e.minion",
+	"../data/test3.minion",
+	"../data/test4.minion",
+	//"../data/test4e.minion",
+	//
+}
+
 func TestM2(t *testing.T) {
 	fmt.Println("\n +++ TestM2:\n ==============")
-	content, err := os.ReadFile("../data/test4.minion")
-	if err != nil {
-		fmt.Println("$ File Error: " + err.Error())
-		return
-	}
-	input := string(content)
-
+	var (
+		input   string
+		ib      InputBuffer
+		t0      time.Time
+		t1      time.Time
+		content []byte
+		err     error
+	)
 	for range 10 {
-		t0 := time.Now()
-		MinionRead(input)
-		t1 := time.Now()
-		fmt.Println(t1.Sub(t0))
+		for _, fin := range infiles {
+			content, err = os.ReadFile(fin)
+			if err != nil {
+				fmt.Println("$ File Error: " + err.Error())
+				return
+			}
+			input = string(content)
+			t0 = time.Now()
+			ib = InputBuffer{}
+			ib.Read(input)
+			t1 = time.Now()
+			fmt.Printf("== %s: %s\n", filepath.Base(fin), t1.Sub(t0))
+		}
+		fmt.Println("  ---")
 	}
 	fmt.Println("  ---------------------------------- ")
 
-	doc, e := MinionRead(input)
+	doc, e := ib.Read(input)
+
 	if len(e) == 0 {
 		fmt.Println("  -->")
-		fmt.Println(MinionDump(doc.Item, 0))
+		dump := Dumper()
+		fmt.Println(dump(doc, 0))
 	} else {
 		fmt.Println(" *** Error ***")
 		fmt.Println(e)
